e4fa49f7f5330ecdc36e81b162af799617f682ae
Armin <arminghasemi1000@gmail.com>
2024-09-18 16:50:56 +0200

    add book create view

--- books/models.py
+++ books/models.py
@@ -16,3 +16,4 @@ class Book(models.Model):
 
 
 
+
--- books/views.py
+++ books/views.py
@@ -13,4 +13,9 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
 
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title' , 'author' , 'description' , 'price']
+    template_name = 'books/book_create.html'
+
 
