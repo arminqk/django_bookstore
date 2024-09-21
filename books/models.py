82bbb1ce892fab3c4d9813bb0f607bed7955997c
Armin <arminghasemi1000@gmail.com>
2024-09-21 15:56:38 +0200

    add cover for books

--- books/models.py
+++ books/models.py
@@ -7,6 +7,8 @@ class Book(models.Model):
     description = models.TextField()
     author = models.CharField(max_length=20)
     price = models.DecimalField(decimal_places=2 , max_digits=5)
+    cover = models.ImageField(upload_to='covers/' , blank=True)
+
 
     def __str__(self):
         return self.title
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
 from django.views.generic.base import TemplateView
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,6 @@ urlpatterns = [
 
 
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # only LOCALHOST
+
+
