bb1144d8c9cef4039c0fb34f508edaa6e1570543
Armin <arminghasemi1000@gmail.com>
2024-09-22 15:38:03 +0200

    add comment form to book detail view

--- books/models.py
+++ books/models.py
@@ -21,7 +21,8 @@ class Comment(models.Model):
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
-
+    is_active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
     def __str__(self):
         return self.text
 
--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 from django.views import generic
 from .models import Book
+from .forms import CommentForm
 
 class BookListView(generic.ListView):
     model = Book
@@ -19,7 +20,20 @@ def book_detail_view(request,pk):
     book = get_object_or_404(Book , pk=pk)
     #book comments
     book_comments = book.comments.all()
-    return render(request , 'books/book_detail.html' , {'book':book , 'comments':book_comments})
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+    return render(request , 'books/book_detail.html' , {'book':book ,
+                                                        'comments':book_comments ,
+                                                        'comment_form':comment_form},)
 
 
 
