e4a94feaae8aa4637fa938dc3b2f0df84bba2212
Armin <arminghasemi1000@gmail.com>
2024-09-22 14:39:30 +0200

    add related to book fk in comment model

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book , Comment
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user','book','text','datetime_created')
+
+
 admin.site.register(Book)
+admin.site.register(Comment , CommentAdmin)
+
--- books/models.py
+++ books/models.py
@@ -20,7 +20,7 @@ class Comment(models.Model):
     user = models.ForeignKey(get_user_model() , on_delete=models.CASCADE)
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
-    book = models.ForeignKey(Book , on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return self.text
